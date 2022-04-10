9b1521a96e6bcdacb0ab6777450f686eef49198e
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-04-10 20:38:08 +0200

    Refacto

--- Katas/hashtag-generator/index.js
+++ Katas/hashtag-generator/index.js
@@ -1,13 +1,12 @@
 const generateHashtag = (str) => {
-  
-    if(str.length === 0 || !str.trim()) return false
-    
-      const arrayOfWords = str.split(' ').map( word => {
-      const capitalized =  word.charAt(0).toUpperCase()
-      const rest = word.slice(1,)
-      return capitalized + rest
-    })
-      
-    const result = '#' + arrayOfWords.reduce((acc , curr) => acc += curr)
-    return  result.length < 141 ?  result : false
-  }
+  if (str.length === 0 || !str.trim()) return false;
+
+  const arrayOfWords = str.split(" ").map((word) => {
+    const capitalized = word.charAt(0).toUpperCase();
+    const rest = word.slice(1);
+    return capitalized + rest;
+  });
+
+  const result = "#" + arrayOfWords.reduce((acc, curr) => (acc += curr));
+  return result.length < 141 ? result : false;
+};
