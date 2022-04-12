e34a429d9869e341cb50d73e59f0e9a8ca55fe72
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-04-12 20:25:00 +0200

    Refacto

--- Katas/narcissistic-number/index.js
+++ Katas/narcissistic-number/index.js
@@ -1,8 +1,14 @@
 const narcissistic = (value) => {
-    const valueToArray = Array.from(String(value), n => Number(n))
-    const arrLength = valueToArray.length
-    if(arrLength === 1) return true
-
-    const total = valueToArray.reduce((acc, curr) => acc + Math.pow(curr, arrLength), 0)
-    return total === value ? true : false
+  if (value < 0) {
+    return false;
   }
+  const valueToArray = Array.from(String(value), (n) => Number(n));
+  const arrLength = valueToArray.length;
+  if (arrLength === 1) return true;
+
+  const total = valueToArray.reduce(
+    (acc, curr) => acc + Math.pow(curr, arrLength),
+    0
+  );
+  return total === value;
+};
