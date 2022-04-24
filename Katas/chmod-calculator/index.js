b192bb5f760fea365375b7c4366c19969e8fbd29
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-04-24 11:55:23 +0200

    Typo

--- Katas/chmod-calculator/index.js
+++ Katas/chmod-calculator/index.js
@@ -1,25 +1,25 @@
 const chmodCalculator = (perm) => {
   const regexArr = [/rwx/, /r-x/, /r--/, /---/, /--x/, /rw-/, /-wx/, /-w-/];
-  const octals = ["7", "5", "4", "0", "1", "6", "3", "2"];
+  const octal = ["7", "5", "4", "0", "1", "6", "3", "2"];
   let octalArray = [];
   perm.user === undefined
     ? octalArray.push(0)
     : regexArr.forEach((regex, index) =>
-        perm.user.match(regex) ? octalArray.push(octals[index]) : null
+        perm.user.match(regex) ? octalArray.push(octal[index]) : null
       );
   perm.group === undefined
     ? octalArray.push(0)
     : regexArr.forEach((regex, index) =>
-        perm.group.match(regex) ? octalArray.push(octals[index]) : null
+        perm.group.match(regex) ? octalArray.push(octal[index]) : null
       );
   perm.other === undefined
     ? octalArray.push(0)
     : regexArr.forEach((regex, index) =>
-        perm.other.match(regex) ? octalArray.push(octals[index]) : null
+        perm.other.match(regex) ? octalArray.push(octal[index]) : null
       );
 
-  let displayOctals = "";
-  return octalArray.map((digit) => (displayOctals += digit))[2];
+  let displayOctal = "";
+  return octalArray.map((digit) => (displayOctal += digit))[2];
 };
 
 module.exports = chmodCalculator;
