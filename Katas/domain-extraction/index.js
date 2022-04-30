189febfa1c35a860a226270d469af752ebc8dd12
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-04-30 22:12:12 +0200

    Export

--- Katas/domain-extraction/index.js
+++ Katas/domain-extraction/index.js
@@ -1,6 +1,10 @@
 const domainName = (url) => {
-    const foo = url.replace(/https:\/\/www.|http:\/\/www\.|http:\/\/|https:\/\/|www\./, '')
-    const dotIndex = foo.indexOf('.')
-    
-    return foo.substr(0,dotIndex) 
-  }
+  const foo = url.replace(
+    /https:\/\/www.|http:\/\/www\.|http:\/\/|https:\/\/|www\./,
+    ""
+  );
+  const dotIndex = foo.indexOf(".");
+
+  return foo.substr(0, dotIndex);
+};
+module.exports = domainName
