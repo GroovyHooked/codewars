1acf2caf870dcc121f1dfeab7ec5a51cb2f00a35
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-05-01 14:55:00 +0200

    Work still  in progress

--- Katas/domain-extraction/test.js
+++ Katas/domain-extraction/test.js
@@ -1,13 +1,14 @@
-const extractUrl = (url) => {
+const domainName = (url) => {
   console.log('**********')
   const regex = /http:\/\/|https:\/\//
   !url.match(regex) ? url = 'https://' + url : null
   const urlObj = new URL(url)
   const hostname = urlObj.hostname;
+  console.log('hostname-> ', hostname)
   const dotIndex = hostname.indexOf(".");
-  console.log("dotIndex->", dotIndex);
+  //console.log("dotIndex->", dotIndex);
   const hostnameChars = Array.from(hostname);
-  console.log('hostnameChars-> ', hostnameChars);
+  //console.log('hostnameChars-> ', hostnameChars);
   const dot = ".";
   let indexArray = [];
   let dotIndexes = hostnameChars.indexOf(dot);
@@ -17,35 +18,54 @@ const extractUrl = (url) => {
     dotIndexes = hostnameChars.indexOf(dot, dotIndexes + 1);
   }
   indexArray[0] += 1;
-  console.log("indexArray-> ", indexArray);
+  //console.log("indexArray-> ", indexArray);
   let result = "";
   hostnameChars.forEach((char) => {
     result += char;
   });
   console.log('result-> ', result);
-  console.log('result.slice-> ',result.slice(indexArray[0], indexArray[1]));
-  console.log('result.slice0-> ',result.slice(0, indexArray[0]));
-  console.log('length-> ',result.slice(indexArray[0], indexArray[1]).length)
-  console.log('match-> ', result.slice(indexArray[0], indexArray[1]).match(/com/g))
+  const splitted = result.split('.')
+  console.log('splitted-> ', splitted)
+  let count = 0
+  splitted.forEach(e => {
+    if(e.length <= 2){
+      count++
+    }
+  })
+  console.log('count-> ', count)
+  //console.log('result.slice-> ',result.slice(indexArray[0], indexArray[1]));
+  //console.log('result.slice0-> ',result.slice(0, indexArray[0]));
+  //console.log('length-> ',result.slice(indexArray[0], indexArray[1]).length)
+  //console.log('match-> ', result.slice(indexArray[0], indexArray[1]).match(/com/g))
   const segment = result.slice(indexArray[0], indexArray[1])
-  const regex1 = /com/
-  console.log('regTest-> ', regex1.test(segment))
-  if(segment.length > 2 && !regex1.test(segment)){
-    console.log('result scope1-> ', result.slice(indexArray[0], indexArray[1]))
-    return result.slice(indexArray[0], indexArray[1])
-  } else if (regex1.test(segment) || segment.length < 2){
+  const regex1 = /www/g
+  //console.log('regTest-> ', regex1.test(segment))
+  console.log('indexArray-> ', indexArray)
+  if(indexArray.length === 1) {
+    indexArray[0] -= 1
+    console.log('result scope1-> ', result.slice(0, indexArray[0]))
+    return result.slice(0, indexArray[0])
+  } else if (indexArray.length > 1 && count === 2){
+    console.log()
+    if(regex1.test(regex)){
+      console.log("scope")
+      return result.slice(indexArray[0], indexArray[1])
+    }
     indexArray[0] -= 1
     console.log('result scope2-> ', result.slice(0, indexArray[0]))
     return result.slice(0, indexArray[0])
+  } else if (indexArray.length > 1 && count !== 2) {
+    console.log('result scope3-> ', result.slice(indexArray[0], indexArray[1]))
+    return result.slice(indexArray[0], indexArray[1])
   } else {
     indexArray[0] -= 1
-    console.log('result scope3-> ', result.slice(0, indexArray[0]))
+    console.log('result scope4-> ', result.slice(0, indexArray[0]))
     return result.slice(0, indexArray[0])
   }
 }
-module.exports = extractUrl
-extractUrl("http://google.com")
-extractUrl("www.xakep.ru")
-extractUrl("http://google.co.jp")
-extractUrl("https://youtube.com")
-extractUrl("https://hdxd4fsck7z1u0x8.biz/default.html")
+module.exports = domainName
+domainName("http://google.com")
+domainName("www.xakep.ru")
+domainName("http://google.co.jp")
+domainName("https://youtube.com")
+domainName("https://hdxd4fsck7z1u0x8.biz/default.html")
