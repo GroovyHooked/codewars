12388f41177585b284f20438fc59c99bc6927cf6
Thomas <62775569+GroovyHooked@users.noreply.github.com>
2023-01-15 10:12:54 +0100

    Refacto

--- Katas/paginationhelper/index.js
+++ Katas/paginationhelper/index.js
@@ -19,39 +19,16 @@ PaginationHelper.prototype.pageCount = function() {
 // returns the number of items on the current page. page_index is zero based.
 // this method should return -1 for pageIndex values that are out of range
 PaginationHelper.prototype.pageItemCount = function(pageIndex) {
-  const howManyPages = Math.ceil(this.collection.length / this.itemsPerPage) 
-  if(pageIndex + 1 > howManyPages || pageIndex < 0){
-    return -1
-  } else if(pageIndex + 1 < howManyPages){
-    return this.itemsPerPage
-  } else {
-    let counter = 0
-    for(let i = 0; i < howManyPages - 1; i++){
-      counter+= this.itemsPerPage
-    } 
-    return this.collection.slice(counter).length
-  }
+  const howManyPages = Math.floor(this.collection.length / this.itemsPerPage);
+  if (pageIndex < 0 || pageIndex >= Math.ceil(this.collection.length / this.itemsPerPage)) return -1;
+  if (pageIndex < howManyPages) return this.itemsPerPage;
+  return this.collection.length % this.itemsPerPage;
 }
+
 
 // determines what page an item is on. Zero based indexes
 // this method should return -1 for itemIndex values that are out of range
 PaginationHelper.prototype.pageIndex = function(itemIndex) {
-  if(itemIndex >= this.collection.length || itemIndex < 0 || this.collection.length === 0) return -1
-  if(itemIndex === 0) return 0
-  if(itemIndex === this.itemsPerPage) return 1
-  if(this.itemsPerPage === 1) return itemIndex
-  const howManyPages = Math.ceil(this.collection.length / this.itemsPerPage)
-  for(let i = 1; i <= itemIndex; i++){
-    if(itemIndex === this.itemsPerPage * i){
-      return i
-    }
-  }
-  let counter1 = 0
-  let counter2 = 0
-  for(let i = 0; i <= howManyPages; i++){
-    counter1+= this.itemsPerPage
-    counter2++
-    if(counter1 >= itemIndex) break
-  }
-  return counter2 - 1
+  if (itemIndex < 0 || itemIndex >= this.collection.length) return -1;
+  return Math.floor(itemIndex / this.itemsPerPage);
 }
