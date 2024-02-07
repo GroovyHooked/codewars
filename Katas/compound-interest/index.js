9064737a56a341b23a4306f12c8dc7fd40f503df
Thomas <62775569+GroovyHooked@users.noreply.github.com>
2024-02-07 13:31:33 +0100

    Update index.js

--- Katas/compound-interest/index.js
+++ Katas/compound-interest/index.js
@@ -1,14 +1,10 @@
 function interest(P, r, n) {
-  let withInterest = P + P * r;
-  let base = 0;
+  let withInterest = P + P * r; 
+  let base = 0; 
   for (let i = 2; i <= n; i++) {
-    if (i === 0) {
-      continue;
-    } else {
-      base = withInterest * r;
+      base = withInterest * r; 
       withInterest += base;
     }
-  }
   if (n === 0) return [Math.round(P + P * r * n), Math.round(P + P * r * n)];
   return [Math.round(P + P * r * n), Math.round(withInterest)];
 }
